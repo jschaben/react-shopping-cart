28f861bfbe52615e69c8a52d55c1805e7add919c
Josh schaben <jschaben42@gmail.com>
2020-01-29 16:31:54 -0600

    got everything working again

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
@@ -6,35 +6,43 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import ProductContext from "./contexts/ProductContext";
+import CartContext from "./contexts/CartContext";
 
 function App() {
 	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+	const [cart, setCart] = useState(
+		localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : []
+	);
+useEffect(() => {
+	localStorage.setItem("cart", JSON.stringify(cart));
+ 	}, [cart]);
+	
+	  const [empty, setEmpty] = useState([]);
+
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([...cart, item]);
 	};
+	const removeItem = itemId => {
+		setCart(cart.filter(item => item.id !== itemId));
+	  };
+	
+	  function clearCart() {
+		setCart(empty);
+	  }
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<ProductContext.Provider value={{ products, addItem }}>
+        		<CartContext.Provider value={{ cart, setCart, removeItem, clearCart }}>
+          			<Navigation cart={cart} />
+          {/* Routes */}
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+          <Route path="/cart" component={ShoppingCart} />
+        </CartContext.Provider>
+        <Route exact path="/" component={Products} />
+      </ProductContext.Provider>
 		</div>
 	);
 }
